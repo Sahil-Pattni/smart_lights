f666a819e489af0dcc91ef2ad3403e96ccbe1bdc
Sahil Pattni <sahilpattni97@gmail.com>
2021-07-30 01:22:38 +0400

    Prefilled data JSON for client

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import requests
 
 url = 'http://192.168.1.103:5000/'
-data = { 'ticker': 'ADAUSDT', 'light': '192.168.1.104'}
+data = { 'ticker': 'ADAUSDT', 'light': '192.168.1.104', 'delay': 1.2}
 
 def post(endpoint, data:dict):
     requests.post(f'{url}{endpoint}', json=data)
